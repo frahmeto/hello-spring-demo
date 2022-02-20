df36d488d7156e8532427b47122e7793df3e2843
Fidel Rahmeto <fidyod@gmail.com>
2022-02-20 11:09:12 -0600

    finished ex10

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -24,11 +24,11 @@ public class HelloController {
         return "Goodbye, Spring!";
     }
 
-    // Handles requests of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
-    }
+//    // Handles requests of the form /hello?name=LaunchCode
+//    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+//    public String helloWithQueryParam(@RequestParam String name) {
+//        return "Hello, " + name + "!";
+//    }
 
     // Handles requests of the form /hello/LaunchCode
     @GetMapping("{name}")
@@ -36,13 +36,45 @@ public class HelloController {
         return "Hello, " + name + "!";
     }
 
-    // /hello/form
-    @GetMapping("form")
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    @ResponseBody
+    public static String createMessage(@RequestParam String name, @RequestParam String language){
+
+        String greeting = "";
+        if(name == null){
+            return name = "World";
+        }
+
+        if(language.equals("english")){
+            greeting ="Hello";
+        }else if(language.equals("french")){
+                greeting ="Bonjour";
+        }else if(language.equals("italian")) {
+            greeting = "Buongiorno";
+        }else if(language.equals("german")) {
+            greeting = "Guten Tag";
+        }else if(language.equals("amharic")) {
+            greeting = "Selam new";
+        }
+        return greeting + " " + name;
+    }
+    // /hello/form<
+    //@GetMapping("form")
+    @RequestMapping(value = "form", method = RequestMethod.GET)
+    @ResponseBody
     public String helloForm() {
         return "<html>" +
                 "<body>" +
                 "<form action = '/hello' method = 'post'>" + // submit a request to /hello
-                "<input type = 'text' name = 'name' >" +
+                "<input type = 'text' name = 'name' />" +
+                "<select name = 'language'>" +
+                "<option value = 'english'>English</option>" +
+                "<option value = 'french'>French</option>" +
+                "<option value = 'italian'>Italian</option>" +
+                "<option value = 'german'>German</option>" +
+                "<option value = 'amharic'>Amharic</option>" +
+                "</select>"+
                 "<input type = 'submit' value = 'Greet Me!' >" +
                 "</form>" +
                 "</body>" +
